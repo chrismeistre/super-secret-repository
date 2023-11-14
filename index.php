d9582e6a93cbb520e5b1b64b1b694e4ff3a6df45
chris <chris@cmcs.co.za>
2023-11-14 20:11:19 +0200

    deactivate developer mode

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-  $dev_mode = true;
+  $dev_mode = false;
   if ($dev_mode) {
     $env_file = __DIR__."/.env.dev";
   } else {
     $env_file = __DIR__."/.env";
+  }
+
+  function checkBase64Encoded($str) {
+    return base64_encode(base64_decode($str, true)) === $str;
   }
   
   if (file_exists($env_file) && is_file($env_file)) {
@@ -11,16 +15,13 @@
     $lines = explode("\n",$env);
 
     foreach($lines as $line){
-      $line = base64_decode(trim($line));
+      if (checkBase64Encoded($line)) {
+        $line = base64_decode(trim($line));
+      }
       preg_match("/([^#]+)\=(.*)/",$line,$matches);
       if(isset($matches[2])){
         putenv(trim($line));
       }
-    }
-
-    # testing access - hardcoded for now
-    if ($dev_mode) {
-      putenv("access_key=\"8OkfHPPQtJB2QLFEJGqkC0s9joaduQCgRxP+Zggt\"");
     }
 
     var_dump(getenv());
