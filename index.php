fd78bcdfa05dc3385d5c5a59b94b4a276517a23b
chris <chris@cmcs.co.za>
2023-11-14 19:50:47 +0200

    add configuration file checking

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
-  $env = file_get_contents(__DIR__."/.env");
-  $lines = explode("\n",$env);
+  $env_file = __DIR__."/.env";
+  if (file_exists($env_file) && is_file($env_file)) {
+    $env = file_get_contents();
+    $lines = explode("\n",$env);
 
-  foreach($lines as $line){
-    preg_match("/([^#]+)\=(.*)/",$line,$matches);
-    if(isset($matches[2])){
-      putenv(trim($line));
+    foreach($lines as $line){
+      preg_match("/([^#]+)\=(.*)/",$line,$matches);
+      if(isset($matches[2])){
+        putenv(trim($line));
+      }
     }
-  }
 
-  var_dump(getenv());
+    var_dump(getenv());
+  } else {
+    echo "Cannot find configuration file.\n";
+  }
